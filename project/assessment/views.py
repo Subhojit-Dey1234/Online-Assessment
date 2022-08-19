83af0de9577933b6e9b372dc5b2de3e3da116205
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-08-19 19:14:12 +0530

    Changes

--- project/assessment/views.py
+++ project/assessment/views.py
@@ -122,7 +122,7 @@ class Test_View_Detail_Single(APIView):
             return Response(test_serializer.data,status= status.HTTP_200_OK)
         return Response(test_serializer.errors,status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self,pk):
+    def delete(self,request,pk):
         test = self.get_object(pk)[0]
         test.delete()
         return Response("Deleted Successfully", status=status.HTTP_200_OK)
